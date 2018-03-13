9df1933273c6086a9ea9ee6a39d459957301525b
Ave Danka <danka.danna@gmail.com>
2018-03-13 21:06:46 +0100

    as
    Firstasas

---
+++ src/components/Layout/Layout.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Aux from '../../hoc/Aux';
+//import classes from './Layout.css';
+
+const layout = ( props ) => (
+    <Aux>
+        <div>
+            tbtbtbt, bbbb, hfhjkkll
+        </div>
+        <main className="Container" >
+            {props.children}
+        </main>
+    </Aux>
+);
+
+export default layout;
